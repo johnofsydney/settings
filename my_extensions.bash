25e731fda3debb7fdc2cb90957d0950a1d4f13cc
John Coote <john.coote@fatzebra.com.au>
2019-06-13 16:48:38 +1000

    added z to settings and split some bash stuff out

--- my_extensions.bash
+++ my_extensions.bash
@@ -1,23 +1,8 @@
-export PS1="\[\e[32m\]\w\[\e[m\] \n$ "
-# my old prompt
-# adadapted from https://gist.github.com/srguiwiz/de87bf6355717f0eede5
-# more comprehensive version here: https://github.com/jcgoble3/gitstuff/blob/master/gitprompt.sh
-function git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-}
-function markup_git_branch {
-  if [[ "x$1" = "x" ]]; then
-    echo -e "$1"
-  else
-    if [[ $(git status 2> /dev/null | tail -n1) = "nothing to commit, working tree clean" ]]; then
-      echo -e '\033[1;32m['"$1"']\033[0;0m'
-    else
-      echo -e '\033[1;31m['"$1"']\033[0;0m'
-    fi
-  fi
-}
-export PS1='\n\[\e[32m\]\w\[\e[m\] $(markup_git_branch $(git_branch)) \n$ '
-source ~/.git-completion.bash
+
+
+PROMPT_COMMAND='history -a'
+# appends shell history to histoery on exit, rather than overwrite. maybe future improvements
+
 ###################################
 
 export LSCOLORS=gxfxcxdxbxegedabagacad
@@ -27,7 +12,7 @@ alias ls="ls -G"
 alias ll="ls -laFG"
 alias reloadbash="source ~/.bash_profile"
 alias atombashprofile="atom ~/.bash_profile && atom ~/Projects/John/settings/"
-alias settings="code ~/.bash_profile && code ~/Projects/John/settings/"
+alias settings="code ~/.bash_profile && code ~/.zshrc && code ~/Projects/John/settings/"
 
 alias aa="atom ."
 alias cc="code ."
