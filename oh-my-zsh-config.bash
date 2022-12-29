682bf1bcb0905e2222a200a4d67a5a73585beb07
John Coote <john_coote@rocketmail.com>
2022-12-30 10:16:44 +1100

    remove git plugin

--- oh-my-zsh-config.bash
+++ oh-my-zsh-config.bash
@@ -79,7 +79,7 @@ ZSH_THEME="john-candy-kingdom" # now a symlink in the themes folder
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+# plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
 
