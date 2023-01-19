0611e0e4cc0c7233304606daadc64a7558fef90b
John Coote <john_coote@rocketmail.com>
2023-01-19 16:08:23 +1100

    added os detector

---
+++ os_detector.sh
@@ -0,0 +1,10 @@
+
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Setting specific options for Linux"
+else
+  # not Linux (it me, so probably Mac).
+  echo "Setting specific options for Mac"
+
+  source ~/Projects/John/settings/mac_settings.sh
+fi
