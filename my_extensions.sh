353e91cf5d01537cea6a926ea17023ed8895190c
John Coote <john.coote@sherpa.net.au>
2023-03-16 09:49:13 +1100

    added rectangle config, updates chat script

--- my_extensions.sh
+++ my_extensions.sh
@@ -93,11 +93,10 @@ function chat () {
   curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer ${CHAT_KEY}" \
-  -d '{"prompt": "'"$@"'", "max_tokens": 1000, "model": "text-davinci-003"}' https://api.openai.com/v1/completions\?format=json \
-  | jq ".choices[0].text" \
-  | sed 's/\\n/\n/g' \
-  | sed 's/"$//' \
-  | cat
+  -d '{"prompt": "'"$@"'", "max_tokens": 1000, "model": "text-davinci-003"}' https://api.openai.com/v1/completions\?format=json | \
+  jq ".choices[0].text" | \
+  sed 's/\\n/\n/g' | \
+  sed 's/"$//'
 }
 
 function hh () {
