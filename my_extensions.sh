f5f8f804e0c431b01df48ea7e5a4c730758c317c
John Coote <john.coote@domain.com.au>
2025-06-11 08:39:56 +1000

    some tweaks to spinup function

--- my_extensions.sh
+++ my_extensions.sh
@@ -56,6 +56,7 @@ alias rspec="nocorrect rspec"
 alias config="nocorrect config"
 alias be="bundle exec"
 alias bep="bundle exec rake parallel:load_schema && bundle exec parallel_rspec -o '--profile --tag ~blob_comparison'"
+alias bep-noskip="bundle exec rake parallel:load_schema && bundle exec parallel_rspec -o '--profile'"
 alias bepc="COVERAGE=true bep"
 alias ber="bundle exec rspec"
 alias berdoc="bundle exec rspec  --profile --format=documentation"
@@ -171,17 +172,21 @@ function spinup () {
 
   local main_dir=""
   local second_dir=""
+  local third_dir=""
 
   local console_cmd=""
   local server_cmd=""
   local secondary_cmd=""
+  local third_cmd=""
 
   if [[ "$project" == "real" ]]; then
     main_dir="$HOME/Projects/realhub"
     second_dir="$HOME/Projects/realhub-frontend"
+    third_dir="$HOME/Projects/realhub-templates-frontend"
     console_cmd="bash -l -c 'rails console'"
     server_cmd="bash -l -c 'APP_SERVER=puma rails server -p 3002'"
     secondary_cmd="bash -l -c 'yarn start'"
+    third_cmd="bash -l -c 'yarn start'"
   elif [[ "$project" == "lester" ]]; then
     main_dir="$HOME/Projects/John/lester"
     second_dir="$main_dir" # there is no seperate Frontend for lester
@@ -199,7 +204,7 @@ tell application "iTerm"
   activate
 
   -- Create a new window with the default profile
-  set newWindow to (create window with profile "MaterialDesignColors")
+  set newWindow to (create window with profile "Default")
   delay 0.5
 
   tell current session of newWindow
@@ -208,7 +213,7 @@ tell application "iTerm"
     write text "clear"
 
     delay 0.5
-    set session2 to (split horizontally with profile "MaterialDesignColors")
+    set session2 to (split horizontally with profile "Default")
   end tell
 
   tell session2
@@ -217,7 +222,7 @@ tell application "iTerm"
     write text "$console_cmd"
 
     delay 0.5
-    set session3 to (split horizontally with profile "MaterialDesignColors")
+    set session3 to (split horizontally with profile "Default")
   end tell
 
   tell session3
@@ -226,13 +231,22 @@ tell application "iTerm"
     write text "$server_cmd"
 
     delay 0.5
-    set session4 to (split vertically with profile "MaterialDesignColors")
+    set session4 to (split vertically with profile "Default")
   end tell
 
   tell session4
     write text "cd $second_dir"
     write text "clear"
     write text "$secondary_cmd"
+
+    delay 0.5
+    set session5 to (split vertically with profile "Default")
+  end tell
+
+  tell session5
+    write text "cd $third_dir"
+    write text "clear"
+    write text "$third_cmd"
   end tell
 end tell
 
